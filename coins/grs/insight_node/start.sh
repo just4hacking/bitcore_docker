39df15e9b5fafbc370c94f7952ca4ec7a2c2b047
fallout0128 <68479312+fallout0128@users.noreply.github.com>
2021-03-09 14:06:43 +0300

    save

--- coins/grs/insight_node/start.sh
+++ coins/grs/insight_node/start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-groestlcoind -conf=/root/node.conf;
-
-node /root/insight/node_modules/.bin/insight-bitcore-api;
-
-sleep infinity;
+groestlcoind -conf=/root/node.conf
---
+++ coins/grs/insight_node/start_insight.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node /root/insight/node_modules/.bin/insight-bitcore-api
